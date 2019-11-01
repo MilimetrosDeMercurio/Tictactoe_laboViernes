52a2f6df32af4515c19fe684cadf46a9d3542194
MilimetrosDeMercurio <00225117@uca.edu.sv>
2019-11-01 11:23:51 -0600

    Tictactoe labo extra

--- src/index.js
+++ src/index.js
@@ -72,6 +72,7 @@ class Game extends React.Component {
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
+        //comentario
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             history: history.concat([{
